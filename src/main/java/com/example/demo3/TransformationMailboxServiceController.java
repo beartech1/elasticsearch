bb12aa28cff4dfa24d5e846860d6f0420aa5f0fb
rickcraver76 <rickcraver76@gmail.com>
2022-01-27 22:42:03 -0500

    utilize service

--- src/main/java/com/example/demo3/TransformationMailboxServiceController.java
+++ src/main/java/com/example/demo3/TransformationMailboxServiceController.java
@@ -1,22 +1,19 @@
 package com.example.demo3;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
 import java.util.Map;
 
 @RestController
 public class TransformationMailboxServiceController {
 
-    private final HttpService mockHttpService;
+    private final HttpService httpService;
 
-    public TransformationMailboxServiceController(HttpService mockHttpService) {
-        this.mockHttpService = mockHttpService;
+    public TransformationMailboxServiceController(HttpService httpService) {
+        this.httpService = httpService;
     }
 
     @GetMapping("/")
@@ -28,7 +25,8 @@ public class TransformationMailboxServiceController {
     public boolean insertData(@RequestBody Map<String, String> data) {
         data.forEach((key, value) -> System.out.println(key + ": " + value));
 
+        String result = httpService.PostJson(data);
 
-        return true;
+        return result != null;
     }
 }
